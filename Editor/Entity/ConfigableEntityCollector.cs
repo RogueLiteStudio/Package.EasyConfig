a918728a511294e842ed4a510d42c825ae2ddfda
张丹 <zh880517@163.com>
2024-08-11 18:55:59 +0800

    添加文件列表

--- Editor/Entity/ConfigableEntityCollector.cs
+++ Editor/Entity/ConfigableEntityCollector.cs
@@ -1,14 +1,46 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using UnityEditor;
 using UnityEngine;
 
 namespace EasyConfig
 {
+    [System.Serializable]
+    public class ConfigableItem
+    {
+        public int CatalogIndex;
+        public int IndexInCatalog;
+        public string Path;
+        public string Name;
+        public GUIContent Content;
+    }
+    [System.Serializable]
+    public class ConfigableCatalogItem
+    {
+        public bool Foldout;
+        public string Name;
+        public Texture2D Icon;
+        public List<ConfigableItem> Items = new List<ConfigableItem>();
+    }
     public class ConfigableEntityCollector : ScriptableSingleton<ConfigableEntityCollector>
     {
         internal static bool hasInstance = false;
         [SerializeField]
         internal List<string> entityPaths = new List<string>();
+        [SerializeField]
+        private List<ConfigableCatalogItem> catalogItems;
+        public IReadOnlyList<ConfigableCatalogItem> CatalogItems 
+        {
+            get
+            {
+                if (catalogItems == null || catalogItems.Count == 0)
+                {
+                    RebuildConfigableList();
+                }
+                return catalogItems;
+            }
+        }
 
         public IReadOnlyList<string> EntityPaths => entityPaths;
 
@@ -41,6 +73,87 @@ namespace EasyConfig
                     entityPaths.Add(path);
                 }
             }
+        }
+
+        public void RebuildConfigableList()
+        {
+            if (entityPaths.Count == 0)
+                Refresh();
+            Dictionary<System.Type, int> typeIndex = new Dictionary<System.Type, int>();
+            if (catalogItems == null)
+                catalogItems = new List<ConfigableCatalogItem>();
+            foreach (var path in entityPaths)
+            {
+                var type = AssetDatabase.GetMainAssetTypeAtPath(path);
+                if (!typeIndex.TryGetValue(type, out int index))
+                {
+                    string catalogName = TypeToCatalogName(type);
+                    index = catalogItems.FindIndex(it => it.Name == catalogName);
+                    if (index < 0)
+                    {
+                        index = catalogItems.Count;
+                        catalogItems.Add(new ConfigableCatalogItem 
+                        { 
+                            Name = catalogName,
+                            Icon = AssetPreview.GetMiniTypeThumbnail(type),
+                        });
+                    }
+                }
+                var catalogItem = catalogItems[index];
+                if (!catalogItem.Items.Exists(it=>it.Path == path))
+                {
+                    var item = new ConfigableItem 
+                    { 
+                        Path = path, 
+                        Name = System.IO.Path.GetFileNameWithoutExtension(path),
+                    };
+                    item.Content = new GUIContent(item.Name, catalogItem.Icon, path);
+                    catalogItem.Items.Add(item);
+                }
+            }
+            //清理不存在的
+            if (typeIndex.Count < catalogItems.Count)
+            {
+                var indexs = typeIndex.Values.ToHashSet();
+                for (int i=catalogItems.Count-1; i>=0; --i)
+                {
+                    if (!indexs.Contains(i))
+                    {
+                        catalogItems.RemoveAt(i);
+                    }
+                    else
+                    {
+                        var catalogItem = catalogItems[i];
+                        for (int j=catalogItem.Items.Count-1; i>=0; --i)
+                        {
+                            if (!entityPaths.Contains(catalogItem.Items[j].Path))
+                            {
+                                catalogItem.Items.RemoveAt(j);
+                            }
+                        }
+                    }
+                }
+            }
+            for (int i=0; i<catalogItems.Count; ++i)
+            {
+                var catalogItem = catalogItems[i];
+                catalogItem.Items.Sort((a, b) => string.Compare(a.Name, b.Name));
+                int index = 0;
+                foreach (var item in catalogItem.Items)
+                {
+                    item.CatalogIndex = i;
+                    item.IndexInCatalog = index++;
+                }
+            }
+        }
+        private string TypeToCatalogName(System.Type type)
+        {
+            var attr = type.GetCustomAttribute<ConfigableEntityCatalogAttribute>(true);
+            if (attr != null)
+            {
+                return attr.CatalogName;
+            }
+            return type.Name;
         }
     }
 
--- Editor/Entity/ConfigableEntityListView.cs
+++ Editor/Entity/ConfigableEntityListView.cs
@@ -1,23 +1,158 @@
 ﻿using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace EasyConfig
 {
-    internal class ConfigableItem
+    public class ConfigableEntityListView : IMGUIContainer, INotifyValueChanged<string>
     {
-        public string Path;
-        public string Name;
-        public Texture2D Icon;
-    }
+        private string searchKey;
+        private string selectedPath;
+        private Vector2 scrollPos;
+        private List<ConfigableItem> searchResult = new List<ConfigableItem>();
+        private bool disableScroll = false;
+        private GUIStyle foldoutStyle;
+        public ConfigableEntityListView()
+        {
+            onGUIHandler = OnGUI;
+        }
 
-    internal class ConfigableCatalogItem
-    {
-        public string Name;
-        public List<ConfigableItem> Items;
-    }
-    public class ConfigableEntityListView : IMGUIContainer
-    {
+        public string value
+        {
+            get => selectedPath;
+            set
+            {
+                if (selectedPath == value)
+                    return;
+                string previousValue = selectedPath;
+                selectedPath = value;
+                if (disableScroll)
+                    ScrollToItem(selectedPath);
+                using (ChangeEvent<string> changeEvent = ChangeEvent<string>.GetPooled(previousValue, value))
+                {
+                    changeEvent.target = this;
+                    SendEvent(changeEvent);
+                }
+            }
+        }
 
+        public void ScrollToItem(string path)
+        {
+            int line = 0;
+            foreach (var item in ConfigableEntityCollector.instance.CatalogItems)
+            {
+                int index = item.Items.FindIndex(it => it.Path == path);
+                if (index < 0)
+                {
+                    if (item.Foldout)
+                    {
+                        line += item.Items.Count;
+                    }
+                }
+                else
+                {
+                    item.Foldout = true;
+                    line += index;
+                    break;
+                }
+            }
+            scrollPos.y = line * EditorGUIUtility.singleLineHeight;
+            MarkDirtyRepaint();
+        }
+
+        public void SetValueWithoutNotify(string newValue)
+        {
+            selectedPath = newValue;
+            ScrollToItem(selectedPath);
+        }
+
+        private void DrawSearchField()
+        {
+            EditorGUI.BeginChangeCheck();
+            searchKey = EditorGUILayout.TextField("", searchKey);
+            if (EditorGUI.EndChangeCheck())
+            {
+                searchResult.Clear();
+                if (!string.IsNullOrEmpty(searchKey))
+                {
+                    foreach (var item in ConfigableEntityCollector.instance.CatalogItems)
+                    {
+                        foreach (var subItem in item.Items)
+                        {
+                            if (subItem.Name.Contains(searchKey, System.StringComparison.CurrentCultureIgnoreCase))
+                            {
+                                searchResult.Add(subItem);
+                            }
+                        }
+                    }
+                }
+            }
+            if (!string.IsNullOrEmpty(searchKey) && GUILayout.Button("X", EditorStyles.boldLabel))
+            {
+                searchKey = "";
+                searchResult.Clear();
+                ScrollToItem(selectedPath);
+            }
+        }
+
+
+        private void OnGUI()
+        {
+            using (new GUILayout.HorizontalScope())
+            {
+                DrawSearchField();
+            }
+            using (var scroll = new GUILayout.ScrollViewScope(scrollPos))
+            {
+                scrollPos = scroll.scrollPosition;
+                if (searchResult.Count > 0)
+                {
+                    foreach (var item in searchResult)
+                    {
+                        DrawItem(item);
+                    }
+                }
+                else
+                {
+                    if (foldoutStyle == null)
+                    {
+                        foldoutStyle = new GUIStyle(EditorStyles.foldout);
+                        var offset = foldoutStyle.contentOffset;
+                        offset.x += EditorGUIUtility.singleLineHeight;
+                        foldoutStyle.contentOffset = offset;
+                    }
+                    foreach (var catalog in ConfigableEntityCollector.instance.CatalogItems)
+                    {
+                        catalog.Foldout = EditorGUILayout.Foldout(catalog.Foldout, catalog.Name, true, foldoutStyle);
+                        if (catalog.Foldout)
+                        {
+                            foreach (var item in catalog.Items)
+                            {
+                                DrawItem(item);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void DrawItem(ConfigableItem item)
+        {
+            bool isSelected = selectedPath == item.Path;
+            if (isSelected)
+            {
+                Color color = GUI.backgroundColor;
+                GUI.backgroundColor = Color.blue;
+                EditorGUILayout.LabelField(item.Content, EditorStyles.boldLabel);
+                GUI.backgroundColor = color;
+            }
+            else if(GUILayout.Button(item.Content, EditorStyles.label))
+            {
+                disableScroll = true;
+                value = item.Path;
+                disableScroll = false;
+            }
+        }
     }
 }
